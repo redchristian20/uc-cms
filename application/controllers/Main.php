e026d6157210fd1e7f9b5436baaf00185633ffb5
Christian Balidoy <redchristian20@gmail.com>
2022-07-22 22:11:47 +0800

    add views

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -12,18 +12,49 @@ class Main extends CI_Controller {
 
 	public function index()
 	{
-        if ( ! file_exists(APPPATH.'views/main.php'))
+        if ( ! file_exists(APPPATH.'views/home.php'))
         {
                 // Whoops, we don't have a page for that!
                 show_404();
         }
 		$this->load->view('header');
-        $this->load->view('main');
+        $this->load->view('home');
 	}
 
-    public function home()
+    public function view($page)
 	{
-		$this->load->view('header');
-        $this->load->view('main');
+
+        if (!file_exists(APPPATH.'views/'.$page.'.php'))
+        {
+            // Whoops, we don't have a page for that!
+            show_404();
+        }
+        switch($page)
+        {
+            case 'home':
+                $this->load->view('header');
+                $this->load->view($page);
+                break;
+            case 'events':
+                $this->load->view('header');
+                $this->load->view($page);
+                break;
+            case 'certificate_verification':
+                $this->load->view('header');
+                $this->load->view($page);
+                break;
+            case 'create_event':
+                $this->load->view('header');
+                $this->load->view($page);
+                break;
+            case 'admin_login':
+                $this->load->view('header');
+                $this->load->view($page);
+                break;
+            default:
+                $this->load->view('header');
+                $this->load->view($page);
+                break;
+        }
 	}
 }
