9b87309e06cd7ba103df6082be0bf42f5fa03e7c
Christian Balidoy <redchristian20@gmail.com>
2022-07-22 21:48:21 +0800

    a

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -2,8 +2,28 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
+    public function __construct()
+    {
+            parent::__construct();
+            $this->load->helper(array('form', 'url'));
+            $this->load->library('csvimport');
+    }
+
+
 	public function index()
 	{
+        if ( ! file_exists(APPPATH.'views/main.php'))
+        {
+                // Whoops, we don't have a page for that!
+                show_404();
+        }
 		$this->load->view('header');
+        $this->load->view('main');
+	}
+
+    public function home()
+	{
+		$this->load->view('header');
+        $this->load->view('main');
 	}
 }
