9ed8e0f479d87a1aefb28271113cc1aeb1813bb7
Christian Balidoy <redchristian20@gmail.com>
2022-08-01 01:28:24 +0800

    Update autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('csvimport','database','session','form_validation');
+$autoload['libraries'] = array('csvimport','database','form_validation');
 
 /*
 | -------------------------------------------------------------------
